02e66d81b56d7fffbe4842835ab9e6405a78acf9
Hiago Mendes <hiagophd@gmail.com>
2024-09-30 20:19:05 -0300

    feat: Create PhoneFrame with tailwind config

---
+++ src/PhoneFrame.tsx
@@ -0,0 +1,13 @@
+const PhoneFrame = () => {
+  return (
+    <div className="border-8 w-[390px] h-[844px] border-red-500 rounded-3xl shadow-lg p-4">
+      <iframe
+        src="https://todo-xarr.onrender.com/"
+        title="Flutter Web App"
+        className="w-full h-full rounded-lg border-none"
+      />
+    </div>
+  );
+};
+
+export default PhoneFrame;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ], theme: {
+
   },
-  plugins: [],
+  plugins: [
+  ],
 }
