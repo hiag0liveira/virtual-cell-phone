37994bac7b12e91f4646b15370d005ffa6880d93
Hiag0liveira <hiagophd@gmail.com>
2024-10-02 22:29:55 -0300

    feat: dragg and drop apps in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,14 +8,22 @@ interface AppData {
 }
 
 const Home: React.FC = () => {
-  const [apps, setApps] = useState<AppData[]>([]);
+  const gridSize = 24;
+  const [apps, setApps] = useState<(AppData | null)[]>([]);
+  const [draggedApp, setDraggedApp] = useState<AppData | null>(null);
   const navigate = useNavigate();
+
   useEffect(() => {
     const loadApps = async () => {
       try {
         const response = await fetch("/apps.json");
         const data: AppData[] = await response.json();
-        setApps(data);
+
+        const initialGrid = [
+          ...data,
+          ...Array(gridSize - data.length).fill(null),
+        ];
+        setApps(initialGrid);
       } catch (error) {
         console.error("Aplicação", error);
       }
@@ -28,20 +36,49 @@ const Home: React.FC = () => {
     navigate(`/app/${app.name}`);
   };
 
+  const handleDragStart = (app: AppData | null) => {
+    setDraggedApp(app);
+  };
+
+  const handleDrop = (targetIndex: number) => {
+    if (draggedApp) {
+      const draggedIndex = apps.findIndex((a) => a?.name === draggedApp.name);
+
+      const newApps = [...apps];
+      newApps[draggedIndex] = newApps[targetIndex];
+      newApps[targetIndex] = draggedApp;
+
+      setApps(newApps);
+      setDraggedApp(null);
+    }
+  };
+
   return (
-    <div className="grid grid-cols-4 gap-2  h-full items-end ">
-      {apps.map((app) => (
+    <div className="grid grid-cols-4 gap-2 h-full">
+      {apps.map((app, index) => (
         <div
-          key={app.name}
-          className="text-center cursor-pointer mt-24"
-          onClick={() => handleOpenApp(app)}
+          key={index}
+          className=" cursor-pointer h-full"
+          draggable={!!app}
+          onDragStart={() => handleDragStart(app)}
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={() => handleDrop(index)}
         >
-          <img
-            src={app.icon}
-            alt={app.name}
-            className="w-16 h-16 mx-auto rounded-lg mb-2"
-          />
-          <p>{app.name}</p>
+          {app ? (
+            <>
+              <div className="flex justify-end items-center flex-col mt-24">
+                <img
+                  src={app.icon}
+                  alt={app.name}
+                  className="w-16 h-16 mx-auto rounded-lg mb-2"
+                  onClick={() => handleOpenApp(app)}
+                />
+                <p>{app.name}</p>
+              </div>
+            </>
+          ) : (
+            <div className="w-16 h-16 mx-auto rounded-lg mb-2 bg-transparent"></div>
+          )}
         </div>
       ))}
     </div>
